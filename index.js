946d6d2f0ce9aa38ee9ea8d843f6bef47a83347e
Chris Jordan <crobertjordan@yahoo.com>
2017-09-08 10:58:46 -0600

    inital setup with mongoose

--- index.js
+++ index.js
@@ -1,34 +1,13 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const port = process.env.PORT || 3000;
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const keys = require('./config/keys.js');
+require('./services/passport');
+const keys = require('./config/keys');
+require('./models/User');
+mongoose.connect(keys.mongoURI);
 
 const app = express();
-
-passport.use(
-  new GoogleStrategy(
-    {
-      clientID: keys.googleClientID,
-      clientSecret: keys.googleClientSecret,
-      callbackURL: '/auth/google/callback'
-    },
-    (accessToken, refreshToken, profile, done) => {
-      console.log(accessToken, 'accessTokent');
-      console.log(refreshToken, 'resreshTok');
-      console.log(profile, 'profile');
-    }
-  )
-);
-
-app.get(
-  '/auth/google/',
-  passport.authenticate('google', {
-    scope: ['profile', 'email']
-  })
-);
-
-app.get('/auth/google/callback', passport.authenticate('google'));
+require('./routes/authRoutes')(app);
 
 app.get('/', (req, res) => {
   res.send({ hi: 'there' });
